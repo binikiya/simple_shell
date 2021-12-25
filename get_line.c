d0b4d353a43c361d27eed8b31da14a96f01f0d79
binikiya <biniyamkiya3682@gamil.com>
2021-12-25 01:39:55 -0800

    Updated Simple Shell

--- get_line.c
+++ get_line.c
@@ -1,4 +1,3 @@
-
 #include "shell.h"
 
 /**
@@ -10,40 +9,38 @@ char *_getline()
 int i, buffsize = BUFSIZE, rd;
 char c = 0;
 char *buff = malloc(buffsize);
-
-	if (buff == NULL)
-	{
-		free(buff);
-		return (NULL);
-	}
-
-	for (i = 0; c != EOF && c != '\n'; i++)
-	{
-		fflush(stdin);
-		rd = read(STDIN_FILENO, &c, 1);
-		if (rd == 0)
-		{
-			free(buff);
-			exit(EXIT_SUCCESS);
-		}
-		buff[i] = c;
-		if (buff[0] == '\n')
-		{
-			free(buff);
-			return ("\0");
-		}
-		if (i >= buffsize)
-		{
-			buff = _realloc(buff, buffsize, buffsize + 1);
-			if (buff == NULL)
-			{
-				return (NULL);
-			}
-		}
-	}
-	buff[i] = '\0';
-	hashtag_handle(buff);
-	return (buff);
+if (buff == NULL)
+{
+free(buff);
+return (NULL);
+}
+for (i = 0; c != EOF && c != '\n'; i++)
+{
+fflush(stdin);
+rd = read(STDIN_FILENO, &c, 1);
+if (rd == 0)
+{
+free(buff);
+exit(EXIT_SUCCESS);
+}
+buff[i] = c;
+if (buff[0] == '\n')
+{
+free(buff);
+return ("\0");
+}
+if (i >= buffsize)
+{
+buff = _realloc(buff, buffsize, buffsize + 1);
+if (buff == NULL)
+{
+return (NULL);
+}
+}
+}
+buff[i] = '\0';
+hashtag_handle(buff);
+return (buff);
 }
 
 /**
@@ -53,14 +50,13 @@ char *buff = malloc(buffsize);
  */
 void hashtag_handle(char *buff)
 {
-	int i;
-
-		for (i = 0; buff[i] != '\0'; i++)
-		{
-			if (buff[i] == '#')
-			{
-			buff[i] = '\0';
-			break;
-			}
-	}
+int i;
+for (i = 0; buff[i] != '\0'; i++)
+{
+if (buff[i] == '#')
+{
+buff[i] = '\0';
+break;
+}
+}
 }
